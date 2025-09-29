9a0b1102261ad9f3b433b45a9d2b11a631ba92c8
Slim Abdelkefi <slimabdelkefi2@gmail.com>
2025-09-29 11:42:50 +0100

    Initial Commit
    File Structure

---
+++ source/src/main.cpp
@@ -0,0 +1,12 @@
+﻿// svgMotion.cpp : Defines the entry point for the application.
+//
+
+#include "svgMotion.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
